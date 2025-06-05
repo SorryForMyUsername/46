797f2993752adede5f2c728fd6039dca5f5829a8
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-06-05 18:13:37 +0300

    Add files via upload

---
+++ Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_5
+{
+    enum Post
+    {
+        Marketer      = 150,
+        HR            = 155,
+        Programmer    = 160,
+        Manager       = 165,
+        Cleaner       = 168,
+        SecurityGuard = 180
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите должность сотрудника:");
+            Post post = (Post)Enum.Parse(typeof(Post), Console.ReadLine());
+            Console.WriteLine("Введите количество отработанных часов:");
+            int hours = int.Parse(Console.ReadLine());
+
+            Accauntant accauntant = new Accauntant();
+
+            if (accauntant.AskForBonus(post, hours))
+            {
+                Console.WriteLine("Сотруднику положена премия.");
+            }
+            else
+            {
+                Console.WriteLine("Сотруднику не положена премия.");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
