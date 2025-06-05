17c8f5b496b24f9834d1648476be0b4ec80c5aba
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-06-05 10:57:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_4
+{
+    internal class Program
+    {
+        enum UserRole { Администратор, Модератор, Пользователь, Гость }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите вашу роль: ");
+            UserRole role;
+
+            if (Enum.TryParse(Console.ReadLine(), out role))
+            {
+                Console.WriteLine($"Вы зарегестрированы как {role.ToString().ToLower()}");
+            }
+            else
+            {
+                Console.WriteLine("Такой роли не существует!");
+            }
+        }
+    }
+}
