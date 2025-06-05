20f8b02bf4eadba21c1dc1a2efa639f03b6c6583
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-06-05 10:26:17 +0300

    Add files via upload

---
+++ Months.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_1
+{
+    enum Months
+    {
+        январь,
+        февраль,
+        март,
+        апрель,
+        май,
+        июнь,
+        июль,
+        август,
+        сентябрь,
+        октябрь,
+        ноябрь,
+        декабрь
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var month in Enum.GetValues(typeof(Months)))
+            {
+                Console.WriteLine($"{(int)month}\t{month}");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
