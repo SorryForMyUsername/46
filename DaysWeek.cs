027c6830f13934b451ce59190cde63e746f8bd83
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-06-05 10:28:37 +0300

    Add files via upload

---
+++ DaysWeek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_2
+{
+    enum DaysWeek
+    {
+        Sat,
+        Sun,
+        Mon,
+        Tue,
+        Wed,
+        Thu,
+        Fri
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var day in Enum.GetValues(typeof(DaysWeek)))
+            {
+                Console.WriteLine($"{day} имеет значение {(int)day}");
+            }
+        }
+    }
+}
